355b41757ec9993f489e887e03600cbb25386067
Jose <josechejo2010@gmail.com>
2023-09-17 17:25:21 -0300

    primer commit

---
+++ indes.js
@@ -0,0 +1,35 @@
+const operaciones = require("./productos")
+
+let array = process.argv.slice(2)
+
+function parsearTerminos(texto) {
+    return {
+        primerTermino: 120,
+        segundoTermino: 890,
+        operacion: "+"
+    }
+}
+
+
+function ejecutarOperacion(oper) {
+    const mapa = {
+        "+": operaciones.sumar,
+        "-": operaciones.resta,
+        "*": operaciones.multiplicar,
+        "/": operaciones.dividir
+    }
+    const simbolo = oper.operacion
+    const ejecutor = mapa[simbolo]
+    return ejecutor(
+        oper.primerTermino,
+        oper.segundoTermino
+    )
+}
+
+function main() {
+    const operacionParseada = parsearTerminos(process.argv[2])
+    const resultado = ejecutarOperacion(operacionParseada)
+    console.log(resultado)
+}
+main()
+console.log("holi")
---
+++ productos.js
@@ -0,0 +1,12 @@
+exports.sumar = function(a, b) {
+    return a + b
+}
+exports.restar = function(a, b) {
+    return a - b
+}
+exports.multiplicar = function(a, b) {
+    return a * b
+}
+exports.dividir = function(a, b) {
+    return a / b
+}
